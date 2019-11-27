8fb3a21de4889d55085b6d6418ea0001614386fd
downey <532720708@qq.com>
2019-11-27 17:45:47 +0800

    增加API

--- bls-api/src/main/java/axj/ap/api/Api_Dev.java
+++ bls-api/src/main/java/axj/ap/api/Api_Dev.java
@@ -1,11 +1,14 @@
 package axj.ap.api;
 
 import axj.an.AEnv;
+import axj.ap.bean.PrayBehavior;
 import axj.ap.bean.Temple;
+import axj.ap.service.PrayCommit;
 import axj.ap.service.TempleList;
 import axj.serv.an.AMapping;
 import axj.serv.an.AServer;
 import axj.serv.an.BBody;
+import com.alibaba.fastjson.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,8 +21,15 @@ import java.util.Map;
 @BBody
 public class Api_Dev  {
 
-    public List<Temple> getTempleList(String text){
-        return TempleList.getTempleList();
+    public List<Temple> getTempleList(String location){
+        if(location != null)
+            return TempleList.getTempleList(location);
+        else
+            return TempleList.getTempleList();
+    }
+
+    public void pray(String prayBehaviorString){
+        PrayCommit.commit(prayBehaviorString);
     }
 
 }
--- bls-bas/src/main/java/axj/ap/service/PrayCommit.java
+++ bls-bas/src/main/java/axj/ap/service/PrayCommit.java
@@ -2,6 +2,7 @@ package axj.ap.service;
 
 import axj.ap.bean.PrayBehavior;
 import axj.db.OSess;
+import com.alibaba.fastjson.JSONObject;
 
 public class PrayCommit {
     public static void commit(PrayBehavior action){
@@ -14,6 +15,26 @@ public class PrayCommit {
         }finally {
             sess.close();
         }
+    }
 
+    public static void commit(String action){
+        PrayBehavior prayBehavior = new PrayBehavior();
+
+        JSONObject jsonObject = JSONObject.parseObject(action);
+        prayBehavior.setUserId(jsonObject.getInteger("userId"));
+        prayBehavior.setKind(jsonObject.getString("kind"));
+        prayBehavior.setName(jsonObject.getString("name"));
+        prayBehavior.setContent(jsonObject.getString("content"));
+        prayBehavior.setGender(jsonObject.getString("gender"));
+
+        OSess sess = OSess.source(null).openSess();
+        try{
+            OSess.Sor<PrayBehavior> sor = sess.insert(null,prayBehavior);
+            sor.update();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            sess.close();
+        }
     }
 }
