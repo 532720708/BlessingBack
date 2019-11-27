7539a29e3577346e622fdd6b0477b61a373552e4
YuanHuanghui <842131145@qq.com>
2019-11-27 15:38:52 +0800

    api测试

--- bls-api/src/main/java/axj/ap/api/Api_Dev.java
+++ bls-api/src/main/java/axj/ap/api/Api_Dev.java
@@ -5,9 +5,26 @@ import axj.serv.an.AMapping;
 import axj.serv.an.AServer;
 import axj.serv.an.BBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @AEnv
 @AServer
 @AMapping("api/dev")
 @BBody
-public class Api_Dev {
+public class Api_Dev  {
+
+    public Map<String, Object> bless(String text) {
+        System.out.println("有了" + text);
+        Map<String, Object> rMap = new HashMap<>();
+        rMap.put("msg", "接收到了");
+        rMap.put("msg1", "接收到了1");
+        rMap.put("msg2", "接收到了2");
+
+        return rMap;
+    }
+
+    public void none() {
+        System.out.println("我是none");
+    }
 }
